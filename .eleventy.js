9da1581e971ab45a84805a9b92394d8d3598e45f
Matt Buck <matt@voxable.io>
2022-04-19 16:23:31 -0400

    Resolve issues with build

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const EleventyManager = require('./src/lib/eleventy-manager')
+const slinkity = require('slinkity')
+const react = require('@slinkity/renderer-react')
 
 module.exports = function (eleventyConfig) {
   const manager = new EleventyManager()
@@ -11,8 +13,22 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.ignores.add("_site/terms/*.md");
 
+  eleventyConfig.addPlugin(slinkity.plugin, slinkity.defineConfig({
+    renderers: [react],
+  }))
+
+  /**
+   * Why copy the /public directory?
+   *
+   * Slinkity uses Vite (https://vitejs.dev) under the hood for processing styles and JS resources
+   * This tool encourages a /public directory for your static assets like social images
+   * To ensure this directory is discoverable by Vite, we copy it to our 11ty build output like so:
+   */
+  eleventyConfig.addPassthroughCopy('public')
+
   const result = manager.initialize(eleventyConfig)
 
-  console.info('JSON: ' + JSON.stringify(result, null, 2))
+  // console.info('JSON: ' + JSON.stringify(result, null, 2))
+
   return result
 }
